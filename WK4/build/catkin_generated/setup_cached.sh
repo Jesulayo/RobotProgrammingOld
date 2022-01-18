2e8682ac94f9086fb904698725df0ec442350830
Computing <ghostye@hotmail.com>
2022-01-18 18:40:29 +0000

    Add files

---
+++ WK4/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/computing/Downloads/WK4/devel:$CMAKE_PREFIX_PATH"
+export GAZEBO_MODEL_PATH="/tmp/binarydeb/ros-melodic-uol-cmp9767m-base-0.7.1/models:/opt/ros/melodic/share/uol_cmp9767m_base/models:/tmp/binarydeb/ros-melodic-bacchus-gazebo-0.3.4/models:/opt/ros/melodic/share/bacchus_gazebo/models:$GAZEBO_MODEL_PATH"
+export LD_LIBRARY_PATH="/home/computing/Downloads/WK4/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/computing/Downloads/WK4/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/computing/Downloads/WK4/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/computing/Downloads/WK4/src:$ROS_PACKAGE_PATH"
---
+++ WK4/build/opencv_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/computing/Downloads/WK4/src/opencv_pkg
+# Build directory: /home/computing/Downloads/WK4/build/opencv_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
